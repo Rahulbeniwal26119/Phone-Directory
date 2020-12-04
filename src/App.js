1084f82027f27581d931cf5f1a8cad5b3f2d67a9
Rahul Beniwal <rahulbeniwal26119@gmail.com>
2020-12-04 20:29:07 +0530

    adding header component and adding styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-
+import Header from './Header'
 
 function App() {
   return (
     <div>
-      phone directory
+    <Header/>
+      <button> Add</button>
+      <div>
+        <span> Name </span>
+        <br />
+        <span> phone</span>
+      </div>
+      {/* <label htmlFor="">Name</label>
+      <input id="name" type="text" placeholder="Enter name" defaultValue="Rahul"/> */}
+      
+
     </div>
   );
 }
