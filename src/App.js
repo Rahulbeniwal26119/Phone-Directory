80d9e4de154b2a9857ce8f34b4c0cf4659c538bf
Rahul Beniwal <rahulbeniwal26119@gmail.com>
2020-12-05 08:09:02 +0530

    adding subscriber dynamically

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
 import Header from './Header'
+import './App.css';
 
 function App() {
+  let subscribers = [
+    {id : 1,
+      name : "Rahul",
+      phone : "05662"
+    },
+    { id : 2,
+      name : "kuldeep",
+      phone : "88888888888"
+    }
+  ]
   return (
-    <div>
+    <div className="component-container">
     <Header/>
-      <button> Add</button>
-      <div>
-        <span> Name </span>
-        <br />
-        <span> phone</span>
-      </div>
-      {/* <label htmlFor="">Name</label>
-      <input id="name" type="text" placeholder="Enter name" defaultValue="Rahul"/> */}
-      
+    <div className="component-body-container">
+          <button className="custom-btn add-btn">Add</button>
 
+          <div className="grid-container heading-container">
+            <span className="grid-item name-heading">Name</span>
+            <span className="grid-item phone-heading">Phone</span>
+          </div>
+          {
+            subscribers.map(sub => {
+              return  <div key={sub.id} className="grid-container">
+            <span className="grid-item ">{sub.name}</span>
+            <span className="grid-item ">{sub.phone}</span>
+          </div>
+            })
+          }
+
+
+        </div>
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React , {Component} from 'react';
+import './Header.css';
+
+const Header = function(){
+
+    return (
+     <div className="header">
+        Phone Directory
+     </div>
+    )
+}
+
+
+export default Header;
